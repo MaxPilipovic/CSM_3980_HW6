96f4f8573db7e1b96c0860008b909c154a34c730
MaxPilipovic <maxpkusa@gmail.com>
2024-03-04 11:36:46 -0800

    Test

---
+++ Main.cu
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "Main.cuh"
+
+__global__ void mykernel(void) {}
+
+int main(void) {
+    mykernel<<<1,1>>>();
+    printf("Hello World!\n");
+    return 0;
+}
+
---
+++ Main.cuh
@@ -0,0 +1,14 @@
+//
+// Created by Max on 3/1/2024.
+//
+
+#ifndef CSM_3980_HW6_MAIN_CUH
+#define CSM_3980_HW6_MAIN_CUH
+
+
+class Main {
+
+};
+
+
+#endif //CSM_3980_HW6_MAIN_CUH
