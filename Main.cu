dd81e507444a2a076a5b38396dc833e88c1590cd
MaxPilipovic <maxpkusa@gmail.com>
2024-03-07 11:01:50 -0800

    - Test Main2.cu

--- Main.cu
+++ Main.cu
@@ -2,14 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 //Single-Threaded Program
-
-void random(int *array, int SIZE) {
-    for (int i = 0; i < SIZE; i++) {
-        array[i] = rand();
-    }
-}
-
 int main() {
+    srand(time(NULL));
     int SIZE = 536870912;
     int *x = (int*)malloc(SIZE * sizeof(int));
     int *y = (int*)malloc(SIZE * sizeof(int));
@@ -20,21 +14,21 @@ int main() {
 
     //Number between 1 and 100
     int c = rand() % 100 + 1;
+    clock_t start_t, end_t;
+    double total_t;
 
-    for (int j = 0; j < 0xFFFFFFF; j++) {
-        int ticks = clock();
-
-        for (int i = 0; i < SIZE; i++) {
-            z[i] = x[i] * c + y[i];
-        }
-        printf("%d\n", c);
-        printf("%d\n", x[3]);
-        printf("Hello World!TEST\n");
-        printf("\n");
-        printf("%f\n", (float)ticks / CLOCKS_PER_SEC);
-        break;
+    start_t = clock();
+    for (int i = 0; i < SIZE; i++) {
+        z[i] = x[i] * c + y[i];
     }
+    end_t = clock();
+    total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("%f\n", total_t);
 
+    printf("%d\n", c);
+    printf("%d\n", x[3]);
+    printf("Hello World!TEST\n");
+    printf("\n");
 
     //Free Memory
     free(x);
@@ -42,5 +36,11 @@ int main() {
     free(z);
 
     return 0;
+}
+
+void random(int *array, int SIZE) {
+    for (int i = 0; i < SIZE; i++) {
+        array[i] = rand();
+    }
 }
 
