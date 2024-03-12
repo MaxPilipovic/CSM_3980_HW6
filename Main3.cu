1a67d168c1e541e8a6c4a7063f4a815f50c60e68
MaxPilipovic <maxpkusa@gmail.com>
2024-03-12 09:33:10 -0700

    - TESTING 3 GB

--- Main3.cu
+++ Main3.cu
@@ -74,7 +74,7 @@ void vecadd(int* x, int* y, int* z, int c, int SIZE) {
 int main() {
     srand(time(NULL));
     //268435456
-    int SIZE = 536870912;
+    int SIZE = 805306368;
     int *x = (int*)malloc(SIZE * sizeof(int));
     int *y = (int*)malloc(SIZE * sizeof(int));
     int *z = (int*)malloc(SIZE * sizeof(int));
