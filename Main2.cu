b82661433b6be495423826cb963da7bb116dacb2
MaxPilipovic <maxpkusa@gmail.com>
2024-03-13 11:41:19 -0700

    - SIZE

--- Main2.cu
+++ Main2.cu
@@ -2,33 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 //Adjacent Multi-Threaded CUDA
-void random(int *array, int size_t) {
-    for (int i = 0; i < size_t; i++) {
+void random(int *array, int SIZE) {
+    for (int i = 0; i < SIZE; i++) {
         array[i] = rand();
     }
 }
 
-__global__ void vecadd_kernel(int* x, int* y, int* z, int c, size_t) {
+__global__ void vecadd_kernel(int* x, int* y, int* z, int c, int n) {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
 
     //Works on different array elements seperated by stride
-    for (int j = i; j < size_t; j += stride) {
+    for (int j = i; j < n; j += stride) {
         z[j] = c * x[j] + y[j];
     }
 }
 
-void vecadd(int* x, int* y, int* z, int c, size_t) {
+void vecadd(int* x, int* y, int* z, int c, int SIZE) {
     //Allocate GPU memory
     int *x_d, *y_d, *z_d;
 
-    cudaMalloc((void**) &x_d, size_t*sizeof(int));
-    cudaMalloc((void**) &y_d, size_t*sizeof(int));
-    cudaMalloc((void**) &z_d, size_t*sizeof(int));
+    cudaMalloc((void**) &x_d, SIZE*sizeof(int));
+    cudaMalloc((void**) &y_d, SIZE*sizeof(int));
+    cudaMalloc((void**) &z_d, SIZE*sizeof(int));
 
     //Copy data to GPU memory
-    cudaMemcpy(x_d, x, size_t*sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(y_d, y, size_t*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(x_d, x, SIZE*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(y_d, y, SIZE*sizeof(int), cudaMemcpyHostToDevice);
 
     //Perform computation on GPU
     int numThreadsPerBlock = 512;
@@ -41,7 +41,7 @@ void vecadd(int* x, int* y, int* z, int c, size_t) {
     cudaEventRecord(start, 0);
     float time;
 
-    vecadd_kernel<<<numBlocks, numThreadsPerBlock>>>(x_d, y_d, z_d, c, size_t);
+    vecadd_kernel<<<numBlocks, numThreadsPerBlock>>>(x_d, y_d, z_d, c, SIZE);
 
     //End time
     cudaEventRecord(stop, 0);
@@ -68,19 +68,19 @@ int main() {
     //268435456
     //805306368
     //1073741824
-    size_t = 1610612736;
-    int *x = (int*)malloc(size_t * sizeof(int));
+    int SIZE = 1610612736;
+    int *x = (int*)malloc(SIZE * sizeof(int));
     int *y = (int*)malloc(SIZE * sizeof(int));
     int *z = (int*)malloc(SIZE * sizeof(int));
 
-    random(x, size_t);
-    random(y, size_t);
+    random(x, SIZE);
+    random(y, SIZE);
 
     //Number between 1 and 100
     int c = rand() % 100 + 1;
 
     //Send it
-    vecadd(x, y, z, c, size_t);
+    vecadd(x, y, z, c, SIZE);
 
     free(x);
     free(y);
